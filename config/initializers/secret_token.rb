054b9ebacd993ff2aa5554ed38c1a5231e28bee8
Dave Kilian <dave.kilian@gmail.com>
2012-01-11 15:14:01 -0500

    Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dixie::Application.config.secret_token = '07f26464585db103004c9b1192461354181e56c4b9cdf5e8795e1c82f55643a168018e59da2af4f5028d3498f6a4457859e464452fd944ae8f57032411e11ef9'
