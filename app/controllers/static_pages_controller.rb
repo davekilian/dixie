a30812b4e62e255f5581dec132e728f1363224d3
Dave Kilian <dave.kilian@gmail.com>
2012-01-22 00:41:56 -0500

    Removed temporary authentication code from the static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,6 @@
 
 class StaticPagesController < ApplicationController
 
-	before_filter :link_to_dropbox, :only => :about
-
 	def about
 	end
 
@@ -10,19 +8,6 @@ class StaticPagesController < ApplicationController
 	end
 
 	def privacy
-	end
-
-private
-
-	require 'dribble.rb'
-	def link_to_dropbox
-		@message = "Default"
-		Dribble::configure(DROPBOX_API_KEY, DROPBOX_API_SECRET, :app_folder, self)
-		Dribble::authorize do |result|
-			result.success { @message = Dribble::client.account_info }
-			result.failure { |code| @message = "Failed with static code #{code}" }
-			result.cached  { @message = "App was already authorized" }
-		end
 	end
 
 end
